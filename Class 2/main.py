3eb7af13b56183bad8a57b29dbf6b22e26e08a55
Danilo Scipioni <dscipioni@ciandt.com>
2025-02-06 10:49:06 -0300

    first project commit

---
+++ Class 2/main.py
@@ -0,0 +1,93 @@
+from langchain_core.prompts import ChatPromptTemplate
+
+from langchain_openai import ChatOpenAI
+import requests
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+flow_api_url = os.getenv("FLOW_API_URL")
+flow_api_url_token = os.getenv("FLOW_API_URL_TOKEN")
+flow_api_client_id = os.getenv("CLIENT_ID")
+flow_api_client_secret = os.getenv("CLIENT_SECRET")
+flow_api_tenant = os.getenv("FLOW_TENANT")
+flow_api_agent = os.getenv("FLOW_AGENT")
+flow_apps = os.getenv("APPS")
+
+
+# FLOW_API_URL_TOKEN=https://flow.ciandt.com/auth-engine-api/v1/api-key/token
+# FLOW_API_URL=https://flow.ciandt.com/ai-orchestration-api/v1/openai
+# CLIENT_ID=**********
+# CLIENT_SECRET=**********
+# FLOW_TENANT=**********
+# FLOW_AGENT=**********
+# APPS=**********
+
+
+def getToken() -> object:
+
+    payload = {
+        "clientId": flow_api_client_id,
+        "clientSecret": flow_api_client_secret,
+        "appToAccess": flow_apps
+    }
+
+    headers = {
+        'Content-Type': 'application/json',
+        'accept': '*/*',
+        'FlowAgent': flow_api_agent,
+        'FlowTenant': flow_api_tenant
+    }
+
+    # Fazendo a requisição POST
+    response = requests.post(flow_api_url_token, json=payload, headers=headers)
+
+    # Verificando o status da resposta
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print('Falha na requisição:', response.status_code)
+        print('Mensagem:', response.text)
+
+# MODELS ALLOWED  'gpt-4-32k', 'gpt-4-0314', 'gpt-4-32k-0314', 'gpt-4o', 'gpt-4o-mini', 'gpt-35-turbo', 'gpt-35-turbo-instruct', 'gpt-35-turbo-16k', 'text-embedding-ada-002', 'o1-mini', 'o1-preview'
+
+
+def generate_company_name(segmento):
+    token = getToken()["access_token"]
+    expires_in = getToken()["expires_in"]
+    try:
+        llm = ChatOpenAI(
+            openai_api_base=flow_api_url,
+            default_headers={
+                "FlowTenant": flow_api_tenant,
+                "FlowAgent": flow_api_agent,
+                "Authorization": f"Bearer {token}",
+            },
+            max_retries=2,
+            n=1,
+            temperature=1,
+            model="gpt-4o",
+            request_timeout=100000
+        )
+
+        template = ChatPromptTemplate([
+            ("system", "Você é um assistente IA que sempre responde em português"),
+            ("human",
+             "Me de 5 sugestoes de nomes de empresas do segmento {segmento} que sejam criativas!"),
+        ])
+
+        prompt_value = template.invoke(
+            {
+                "segmento": segmento,
+            }
+        )
+
+        return llm.invoke(prompt_value).content
+
+    except Exception as e:
+        print("Ocorreu um erro", e)
+
+
+if __name__ == "__main__":
+    print(generate_company_name("estética"))
